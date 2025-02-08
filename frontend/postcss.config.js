d9bbce56df85997b3983fe06139cf6beb1c6aa5c
dominguesh <dominguesh@gmail.com>
2025-02-08 10:20:08 -0700

    installing the PostCSS dependencies globally, correct error with component imports

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   plugins: {
-    'tailwindcss': {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': true
-      }
-    },
-    'autoprefixer': {}
-  }
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
